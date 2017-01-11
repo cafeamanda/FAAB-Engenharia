c76b7f3d0287cffab8b9066baca29dfd7184373d
Amanda de Araujo <amandadearaujo0@gmail.com>
2017-01-11 16:29:10 -0200

    All pages & links up 'n running

---
+++ scripts/snap.js
@@ -0,0 +1,7 @@
+$('html, body').css({
+  'overflow-x':'hidden',
+  '-webkit-scroll-snap-type':'mandatory',
+  'scroll-snap-type':'mandatory',
+  '-webkit-scroll-snap-points-y':'repeat(100%)',
+  'scroll-snap-points-y':'repeat(100%)'
+});
