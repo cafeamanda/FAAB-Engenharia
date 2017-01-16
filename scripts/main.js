aa6e9d25cbeb1fe46b7650ec23f27bf7415f6b88
Amanda de Araujo <amandadearaujo0@gmail.com>
2017-01-16 17:28:49 -0200

    Refactoring

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,29 @@
+/**
+*   @author Amanda Café <contato@cafeamanda.com.br>
+*   @description custom javascript source code file for FAAB Engenharia's website.
+*
+**/
+
 $(document).ready(function() {
 
+  // hide smaller navbar-brand on all pages
+  $('#logoSymbol').hide();
+
+  // hide toggleable content @ OBRAS <obras.html>
+  $('#maisExec').hide();
+  $('#maisEmExec').hide();
+
+  // Initiate tooltips
+  $('[data-toggle="tooltip"]').tooltip();
+
+  // Initiate Carousel
   $('.carousel').carousel({
      interval: 4000,
      cycle: true
    });
 
-  $('#logoSymbol').hide();
-
-  $('[data-toggle="tooltip"]').tooltip();
-
-  var onResize = function() {
-    var navbarHeight = $('.navbar-fixed-top').height();
-    $('#j1').stop().animate({
-      paddingTop:navbarHeight
-    }, 300);
-  };
-
-  $(window).resize(onResize);
-  $(function() {onResize()});
-
+  // Navbar gets smaller when window.scrollTop() > 0
   $(window).scroll(function(event){
-
     var scroll = $(this).scrollTop();
     if (scroll > 0){
       $('#navLogo').hide(300);
@@ -31,21 +34,44 @@ $(document).ready(function() {
       $('.navbar').css('padding-top','10px');
       $('#logoSymbol').hide(100);
     }
-    onResize();
   });
 
+  // Show #menu @ HOME <index.html> when mouse hovers the 'plus' sign @ the bottom right corner of the page
   $('#menuWrapper').hover(function() {
     $('#menu').css('display','block');
     $('#menuIconWrapper').css('opacity','0.8');
   });
 
-  $('.jumbotron').hover(function(event) {
+  // Close #menu on mouseleave
+  $('.jumbotron, footer').hover(function(event) {
     if ($('#menu').css('display')=='block' && !$(event.target).hasClass('showMenu')) {
       $('#menu').css('display','none');
       $('#menuIconWrapper').css('opacity','0.5');
     }
   });
 
+  // Animate CSS changes in #menu on hover
+  $('#menu ul li').unbind('mouseenter mouseleave'); // Unbind 'hover' back to 'mousenter' and 'mouseleave'
+  $('#menu ul li').on('mouseenter', function() {
+    $(this).css({
+      'background':'#32336b',
+      'color':'#eee',
+    });
+    $(this).stop().animate({
+      paddingLeft:25
+    },100);
+  });
+  $('#menu ul li').on('mouseleave', function() {
+    $(this).css({
+      'background':'#eee',
+      'color':'#32336b',
+    });
+    $(this).stop().animate({
+      paddingLeft:20
+    },100);
+  });
+
+  // Scroll to designated jumbotron on click @ HOME <index.html>
   $('#inicio').click(function() {
     $('html, body').animate({
       scrollTop: $('#j1').offset().top
@@ -71,8 +97,14 @@ $(document).ready(function() {
       scrollTop: $('#j5').offset().top
     }, 500);
   });
+  $('#rodape').click(function() {
+    $('html, body').animate({
+      scrollTop: $('#home footer').offset().top
+    }, 500);
+  });
 
-  $('footer p').unbind('mouseenter mouseleave');
+  // Animate CSS changes in 'footer' on hover
+  $('footer p').unbind('mouseenter mouseleave'); // Unbind 'hover' back to 'mousenter' and 'mouseleave'
   $('footer p').on('mouseenter', function() {
       $(this).stop().animate({
         paddingLeft:5
@@ -84,21 +116,8 @@ $(document).ready(function() {
     }, 100);
   });
 
-  $('.chamada').unbind('mouseenter mouseleave');
-  $('.chamada').on('mouseenter', function() {
-      $(this).stop().animate({
-        paddingLeft:40
-      }, 100);
-  });
-  $('.chamada').on('mouseleave', function() {
-    $(this).stop().animate({
-      paddingLeft:0
-    }, 100);
-  });
-
-  $('#maisExec').hide();
-  $('#maisEmExec').hide();
-
+  // hide() and show() toggleable content @ OBRAS <obras.html>
+  // change font-awesome icon from 'plus' to 'minus' and vice versa
   $('#maisEmExecBtn').on('click', function() {
     if ($(this).hasClass('fa-plus')==true) {
       $(this).removeClass('fa-plus').addClass('fa-minus');
@@ -109,7 +128,6 @@ $(document).ready(function() {
       $('#maisEmExec').hide(200);
     }
   });
-
   $('#maisExecBtn').on('click', function() {
     if ($(this).hasClass('fa-plus')==true) {
       $(this).removeClass('fa-plus').addClass('fa-minus');
